beb018cfcb9b2eec5218cd570f61e3487e7b8993
aysenurcekic <109110713+aysenurcekic@users.noreply.github.com>
2022-08-04 22:17:16 +0300

    Add files via upload

---
+++ odev1/index.js
@@ -0,0 +1,13 @@
+function name(){
+    var name = prompt("Adınızı giriniz", "John Doe");
+    var namedoc = document.getElementById("myName");
+    namedoc.innerHTML = name;
+}
+
+function showTime(){
+    let date = new Date().toLocaleString('tr-TR'); 
+   document.getElementById('myClock').innerHTML=date;
+   setTimeout(showTime, 1000);  
+}
+
+setInterval(showTime, 1000);
